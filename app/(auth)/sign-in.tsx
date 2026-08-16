4b0aad5970d11b075cbb45b4dcc681adeb4e3f11
Mohit Saini <mohitsainimrdbd@gmail.com>
2026-08-16 14:10:38 +0530

    first commit

---
+++ app/(auth)/sign-in.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Link } from 'expo-router'
+
+const SignIn = () => {
+  return (
+    <View>
+      <Text>Sign In</Text>
+      <Link href="/(auth)/sign-up">Create Account</Link>
+    </View>
+  )
+}
+
+export default SignIn
